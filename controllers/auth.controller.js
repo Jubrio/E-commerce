60ce415260eeb0277aa23c21fbe25a17a3684a5d
Safidi16 <safidylegran@gmail.com>
2026-06-06 20:18:26 +0300

    fix: force IPv4 SMTP

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -41,8 +41,9 @@ async register(req, res) {
     const nodemailer = require('nodemailer');
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
-      port: parseInt(process.env.EMAIL_PORT) || 465,
+      port: 465,
       secure: true,
+      family: 4, 
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
